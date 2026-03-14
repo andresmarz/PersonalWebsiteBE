a8fb4054a9928af60b167fa87a8926dbbd7291f8
andresmarz <andresmarquezzuleta@gmail.com>
2026-03-14 13:14:50 -0400

    Agregar archivos de proyecto.

---
+++ PersonalWebsiteBE/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonalWebsiteBE.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
