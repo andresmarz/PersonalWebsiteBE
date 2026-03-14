60124a2caeca7b5f810f4da74483adb399f2c661
andresmarz <andresmarquezzuleta@gmail.com>
2026-03-14 15:34:29 -0400

    Add Data folder and AppDbContext.cs

---
+++ PersonalWebsiteBE/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalWebsiteBE.Models;
+
+namespace PersonalWebsiteBE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
