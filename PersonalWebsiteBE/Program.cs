7c61512906d050648e52193fd88df4b2a967dc64
andresmarz <andresmarquezzuleta@gmail.com>
2026-03-14 19:51:45 -0400

    Updating connection string on appsettings.json so it can use SQLExpress and doing the migrations

--- PersonalWebsiteBE/Program.cs
+++ PersonalWebsiteBE/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
