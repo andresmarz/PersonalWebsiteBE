5fa2624473145494bf0d86bc50dd8c9160126f37
andresmarz <andresmarquezzuleta@gmail.com>
2026-03-14 20:04:32 -0400

    Adding PostMessage and GetMessages to the controller

--- PersonalWebsiteBE/Controllers/ContactController.cs
+++ PersonalWebsiteBE/Controllers/ContactController.cs
@@ -1,6 +1,39 @@
-﻿namespace PersonalWebsiteBE.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebsiteBE.Data;
+using PersonalWebsiteBE.Models;
+
+namespace PersonalWebsiteBE.Controllers
 {
-    public class ContactController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostMessage(Message message)
+        {
+            message.Fecha = DateTime.UtcNow;
+
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return Ok(message);
+        }
+
+        [HttpGet]
+        public IActionResult GetMessages()
+        {
+            var messages = _context.Messages
+                .OrderByDescending(m => m.Fecha)
+                .ToList();
+
+            return Ok(messages);
+        }
     }
 }
