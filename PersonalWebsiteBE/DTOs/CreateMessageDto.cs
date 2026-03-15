d7ed7b747a28f1d77110f9913b980aecb9c68014
andresmarz <andresmarquezzuleta@gmail.com>
2026-03-14 20:40:54 -0400

    Adding DTOs folder and CreateMessageDto.cs

---
+++ PersonalWebsiteBE/DTOs/CreateMessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalWebsiteBE.DTOs
+{
+    public class CreateMessageDto
+    {
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Mensaje { get; set; }
+    }
+}
